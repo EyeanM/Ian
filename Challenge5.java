649acccd0c3c5b723d2a7fab1960af58bcce91fb
EyeanM <146996297+EyeanM@users.noreply.github.com>
2023-10-16 12:04:51 -0700

    Add files via upload
    Almost finished product

--- Challenge5.java
+++ Challenge5.java
@@ -7,9 +7,11 @@ public class Challenge5 {
         boolean inPlay = true;
         String computerInput;
         int AI;
+        int score = 0;
+        int AIscore = 0;
 
         while (inPlay != false){
-            System.out.println("Rock, Paper, or Scissors? ");
+            System.out.println("Rock, Paper, or Scissors? (Please input full word): ");
             String userInput = scan.nextLine().toLowerCase();
 
             // Computer picking their move
@@ -28,19 +30,27 @@ public class Challenge5 {
             // Determining a winner
             if ((userInput.equals("rock")) && (computerInput.equals("scissors"))){
                 System.out.println("You Win!");
+                score++;
             }
             else if ((userInput.equals("scissors")) && (computerInput.equals("paper"))){
                 System.out.println("You Win!");
+                score++;
             }
             else if ((userInput.equals("paper")) && (computerInput.equals("rock"))) {
                 System.out.println("You Win!");
+                score++;
             }
             else if (userInput.equals(computerInput)){
                 System.out.println("The game is a draw");
             }
             else{
                 System.out.println("You lose.");
+                AIscore++;
             }
+
+            System.out.println();
+            System.out.println("Your score: " + score + "  |  AI Score: " + AIscore);
+            System.out.println();
 
             System.out.println("Would you like to play again? (y or n): ");
             String answer = scan.nextLine().toLowerCase();
